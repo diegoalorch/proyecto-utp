856f387744e0fd05ebfa496bae75fde080ddd76e
Your Name <you@example.com>
2021-11-18 20:48:44 -0500

    errors solutioned

--- js/funciones.js
+++ js/funciones.js
@@ -34,3 +34,19 @@ function Jugar() {
         jugar = true;
     }
 }
+
+function close_window(){
+    Swal.fire({
+    title: 'Estas seguro de que deseas salir?',
+    text: "No podras revertir los cambios!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Se terminara el juego!'
+    }).then((result) => {
+    if (result.isConfirmed) {
+        windows.close();
+    }
+    })
+}
